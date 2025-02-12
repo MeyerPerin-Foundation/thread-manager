c76518a2d657a5c0bb5581e6e442ad1259c55cd1
Lucas Meyer <lucas.meyer@microsoft.com>
2025-02-12 02:09:19 -0600

    Fixed missing requests bug

--- content/too_far/too_far_blueprint.py
+++ content/too_far/too_far_blueprint.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from content.too_far import TooFarContent
 
 too_far_bp = Blueprint('too_far', __name__, url_prefix='/too_far')
@@ -10,6 +10,7 @@ def post_too_far():
 
     # if there's any json data in the request, get its after_utc
     after_utc = request.json.get("after_utc", None)    
+
     if after_utc:
         d = f.generate_too_far(after_utc=after_utc)
         return "Accepted", 202
