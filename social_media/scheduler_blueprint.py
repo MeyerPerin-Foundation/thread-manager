6d1867b5ece44cfead3a81dc5060fc5c90c36602
Lucas Meyer <lucas.meyer@microsoft.com>
2025-02-19 12:21:21 -0600

    Fixed bug in imgflip content

--- social_media/scheduler_blueprint.py
+++ social_media/scheduler_blueprint.py
@@ -33,31 +33,40 @@ def task_list():
 @scheduler_bp.route("/<string:task_id>/edit", methods=["GET", "POST"])
 def edit_task(task_id):
     s = SocialMediaScheduler()
-    task = s.get_task(task_id)
+    # flatten request.form to a dict
+    logger.debug(f"Received task: {request.form}")
+    task_data = s.get_task(task_id)
     if request.method == "POST":
-        task["command"] = request.form["command"]
-        task["repeat_every"] = int(request.form["repeat_every"])
-        task["repeat_unit"] = request.form["repeat_unit"]
-        task["last_scheduled_time_utc"] = request.form["last_scheduled_time_utc"]
-        task["next_scheduled_time_utc"] = request.form["next_scheduled_time_utc"]
-        if "command_parameters" in request.form:
-            task["command_parameters"] = request.form["command_parameters"]
-        s.update_task(task_id, task)
+        task_data["command"] = request.form["command"]
+        task_data["repeat_every"] = int(request.form["repeat_every"])
+        task_data["repeat_unit"] = request.form["repeat_unit"]
+        task_data["last_scheduled_time_utc"] = request.form["last_scheduled_time_utc"]
+        task_data["next_scheduled_time_utc"] = request.form["next_scheduled_time_utc"]
+        # for each key in request.form, if the key starts with "command_parameters.", add it to task_data
+        for key in request.form:
+            if key.startswith("command_parameters_"):
+                # if there's no command_parameters key in task_data, create it
+                if "command_parameters" not in task_data:
+                    task_data["command_parameters"] = {}
+                task_data["command_parameters"][key[len("command_parameters_"):]] = request.form[key] 
+        logger.debug(f"Updating task {task_id}: {task_data}")
+        s.update_task(task_id, task_data)
         return redirect(url_for("scheduler.task_list"))
-    return render_template("task_form.html", task=task)
+    return render_template("task_form.html", task=task_data)
     
 @scheduler_bp.route("/<string:task_id>", methods=["GET"])
 def view_task(task_id):
     s = SocialMediaScheduler()
     try:
-        task = s.get_task(task_id)
-        return render_template("task_detail.html", task=task)
+        task_data = s.get_task(task_id)
+        return render_template("task_detail.html", task=task_data)
     except:
         return "Task not found", 404
 
 @scheduler_bp.route("/new", methods=["GET", "POST"])
 def create_task():
     if request.method == "POST":
+        request.form = request.form.to_dict(flat=True)
         logger.debug(f"Received task: {request.form}")
         task_data = {
             "id": request.form["id"],
@@ -67,8 +76,9 @@ def create_task():
             "last_scheduled_time_utc": request.form["last_scheduled_time_utc"],
             "next_scheduled_time_utc": request.form["next_scheduled_time_utc"],
         }
-        if "command_parameters" in request.form:
-            task_data["command_parameters"] = request.form["command_parameters"]
+        for key in request.form:
+            if key.startswith("command_parameters_"):
+                task_data["command_parameters"][key[len("command_parameters_"):]] = request.form[key] 
         s = SocialMediaScheduler()
         logger.debug(f"Creating task: {task_data}")
         s.create_task(task_data)
